cf0fbbda7b75ce2dd3271e98457f33d8b958c7a6
Tobia righi <splint@protonmail.com>
2017-02-28 16:15:57 +0100

    Added content and google map

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
-function where(){
-  location.href = "#"
-  location.href = "#where"
-  //window.scrollTo(0, 700);
+function initMap() {
+        var summerPlace = {lat: 57.6959833, lng: 11.9743703};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 16,
+          center: summerPlace
+        });
+        var marker = new google.maps.Marker({
+          position: summerPlace,
+          map: map
+        });
 }
